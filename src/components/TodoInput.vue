0cb5f8a4eb1a332bc70298605daa599515c2c7e2
1320129 <shwogur3000@gmail.com>
2021-09-20 21:47:02 +0900

    vue로 todo 만들기

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-      input
+  <div class="inputBox shadow">
+      <input type="text" v-model="inputcont" @keyup.enter="search">
+      <!-- <button @click="search">add</button> -->
+      <span class="addContainer" @click="search">
+          <i class="fas fa-plus addBtn"></i>
+      </span>
   </div>
 </template>
 
 <script>
 export default {
-
+    data:function(){
+        return{
+            inputcont:'',
+        }
+    },
+    methods:{
+        search:function(){
+            let obj = {complete:false, item:this.inputcont};
+            localStorage.setItem(this.inputcont,JSON.stringify(obj));
+            this.clearinput();
+        },
+        clearinput:function(){
+            this.inputcont = '';
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus{
+    outline: none;
+}
+.inputBox{
+    background:white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer{
+    float:right;
+    background:linear-gradient(to right, #6478fb,#8763fb);
+    display:block;
+    width:3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn{
+    color:white;
+    vertical-align: middle;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,77 @@
 <template>
   <div>
-      lsit
+      <ul>
+          <li v-for="(todoitem, index) in todoitems" v-bind:key="index" class="shadow">
+              <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoitem.complete}" @click="toggletodo(todoitem, index)"></i>
+              <span v-bind:class="{textCompleted: todoitem.complete}">{{ todoitem.item }}</span>
+              <span class="removeBtn" @click="removeTodo(todoitem, index)">
+                  <i class="fas fa-trash-alt"></i>
+              </span>
+          </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+    data:function(){
+        return{
+            todoitems:[],
+        }
+    },
+    methods:{
+        removeTodo:function(todoitem, index){
+            localStorage.removeItem(todoitem);
+            this.todoitems.splice(index,1);
+        },
+        toggletodo:function(todoitem){
+            todoitem.complete = !todoitem.complete;
+            localStorage.removeItem(todoitem.item);
+            localStorage.setItem(todoitem.item, JSON.stringify(todoitem));
+        }
+    },
+    created:function(){
+        for(let i = 0;localStorage.length > i; i++){
+            if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+            this.todoitems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+    list-style-type: none;
+    padding-left: 0;
+    margin-top:0;
+    text-align: center;
+}
+li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin:0.5rem 0;
+    padding:0 0.9rem;
+    background:#fff;
+    border-radius: 5px;
+}
+.checkBtn{
+    line-height: 45px;
+    color:#62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted{
+    color:#b3adad;
+}
+.textCompleted{
+    text-decoration: line-through;
+    color:#b3adad;
+}
+.removeBtn{
+    cursor: pointer;
+    margin-left:auto;
+    color:#de4343;
+}
 </style>
